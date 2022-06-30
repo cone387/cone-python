61dd3aa929503e8ceca7049deddb2eeca7089714
bizhongliang <wb-bzl830847@alibaba-inc.com>
2022-06-30 14:43:09 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cone-lib',
+    packages=find_packages(),
+    version='1.0.0',
+    # install_requires=[
+    # ],
+    # extras_require={
+    # },
+    license='MIT',
+    url='https://github.com/cone387/cone-python',
+    python_requires='>=3.8, <4',
+)
